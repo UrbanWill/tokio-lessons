07b8732a0e626baea17225ea43c5fc1d8026331a
UrbanWill <will.urban.dev@gmail.com>
2026-09-25 13:58:25 +0700

    chore: exercise-2 re-add code comment

--- src/assignment-2.rs
+++ src/assignment-2.rs
@@ -64,6 +64,7 @@ async fn main() {
 
     // Spawn a task to collect and print results from the channel
     handles.push(tokio::spawn(async move {
+        // Collect and print results as they arrive
         loop {
             let Some(result) = rx.recv().await else {
                 break;
